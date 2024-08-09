0493b6d874aa5cd1be169a8374c8bb09879843c4
Muhammad Saim Sajid <saimsajidiub@gmail.com>
2024-08-09 17:21:28 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Image-Recognizer-Using-Machine-Learning
+ My machine learning project leverages Streamlit to deploy a real-time image recognizer. Users upload images, triggering a trained model to identify content and unveil hidden patterns within. This user-friendly application bridges the gap between machine learning and accessibility.
---
+++ main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+from PIL import Image
+import tensorflow as tf
+
+def main():
+    st.title("Image Recognizer By Saim Sajid")
+    st.write("Upload any image you want to recognize")
+    file = st.file_uploader("Please Upload an image", type=["jpg", "png"])
+    if file:
+        image = Image.open(file)
+        st.image(image, use_column_width=True)
+        resized_image = image.resize((32, 32))
+        img_array = np.array(resized_image) / 255
+        img_array = img_array.reshape((1, 32, 32, 3))
+        
+        model = tf.keras.models.load_model("cifar10_model.h5")
+        predictions = model.predict(img_array)
+        
+        cifar10_classes = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+        fig, ax = plt.subplots()
+        y_pos = np.arange(len(cifar10_classes))
+        ax.barh(y_pos, predictions[0], align="center")
+        ax.set_yticks(y_pos)
+        ax.set_yticklabels(cifar10_classes)  
+        ax.invert_yaxis()
+        ax.set_xlabel("Probability") 
+        ax.set_title("Cifar10 Predictions")
+        st.pyplot(fig)
+
+    else:
+        st.text("Please Upload An Image")
+
+if __name__ == "__main__":
+    main()
